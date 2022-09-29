df5fe074e989228c6986fa73079dbfa17433ddd6
Kseniia Denisova <kdenisova@8thlight.com>
2022-09-29 11:37:21 -0400

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "ACG-Terraform-Demos-kd"
+    organization = "ACG-Terraform-Demoes-kd"
 
     workspaces {
       name = "demo-github-actions"
